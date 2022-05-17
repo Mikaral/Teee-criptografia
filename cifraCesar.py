8e149d247225e1263f0da45371a6bbbedbee497d
Mikael Amaral <55303570+Mikaral@users.noreply.github.com>
2022-05-17 00:52:27 -0300

    Bug fixing

--- cifraCesar.py
+++ cifraCesar.py
@@ -1,3 +1,4 @@
+from curses.ascii import isalnum
 import enchant
 import string
 
@@ -21,12 +22,11 @@ def caesar(lines, k, mode):
         lineAux = ""
 
         for letter in line:
-            if(letter != " " and letter not in string.punctuation):
+            if(letter in string.ascii_letters):
                 lineAux += string.ascii_letters[(string.ascii_letters.index(letter) % 52) + k]
             else:
                 lineAux += letter
-        
-        
+
         lines[i] = lineAux
     
     return lines
@@ -34,11 +34,13 @@ def caesar(lines, k, mode):
 
 def breakCypher(path):
     k = 1
+    counter = 0
     correctKey = False
     
     while(not correctKey and k < 50):
         decipherAttempt = caesar(getFileLines("text"), k, False)
-        correctKey = all(list(map(lambda x: d.check(x), decipherAttempt[0].split())))
+        correctWords = list(map(lambda x: d.check(x), decipherAttempt[0].strip(string.punctuation).split())).count(True)
+        correctKey = (correctWords >= len(decipherAttempt[0].split()) * 0.75 )
         k += 1
     
     return k - 1
