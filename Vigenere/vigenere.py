fba3dddca9a897006e1ea32f13d33c4963d585f9
Mikael Amaral <55303570+Mikaral@users.noreply.github.com>
2022-05-28 18:18:16 -0300

    Finally added Playfair and Vigenère Ciphers on this shithole

---
+++ Vigenere/vigenere.py
@@ -0,0 +1,92 @@
+import string
+import random
+
+def getFileLines(path):
+    try:
+        f = open(path, "r")
+        lines = list(map(lambda x: x.rstrip().strip("\""), f.readlines()))
+        f.close()
+        return lines
+    except:
+        raise FileNotFoundError("There is no such file with this name")
+
+def writeLines(path, lines):
+    try:
+        f = open(path, "w")
+        for line in lines: f.write(str(line) + "\n")
+        f.close()
+    except:
+        raise Exception("Something went wrong when writing to the file")
+    
+def generateKeys(size = 6):
+    keys = []
+    for i in range(size):
+        keys.append(random.randint(1,25))
+    
+    print(keys)
+    writeLines("key.txt", keys)
+    
+    return keys
+
+def shiftLetter(letter, k):
+    if(letter in string.ascii_letters):
+        letter = string.ascii_letters[(string.ascii_letters.index(letter) + k) % 52]
+    
+    return letter
+
+def vigenere(lines, keys, mode):
+    
+    for i, line in enumerate(lines):
+        lines[i] = caesar(line, int(keys[i % len(keys)]), mode)
+    
+    return lines
+
+def caesar(line, k,  mode):
+    key = k if mode else k*-1
+    lineAux = ""
+
+    for letter in line:
+        lineAux = lineAux + shiftLetter(letter, key)
+    
+    return lineAux
+
+def getUserVariables(mode):
+    key = 0
+    path = input("Write the path of the file: ")
+    if mode:
+        key = input("Write the key size: ")
+
+    return (path, int(key))
+
+def encriptFile(mode):
+    userInputs = getUserVariables(mode)
+    
+    if(mode):
+        print("Writing encrypted text on ../encryptedText.txt")
+        keys = generateKeys(userInputs[1])
+    else:
+        keys = getFileLines("key.txt")
+        print("Writing decrypted text on ../encryptedText.txt")
+    
+
+    lines = vigenere(getFileLines(userInputs[0]), keys, mode)
+    writeLines("encryptedText.txt", lines)
+
+
+def main():
+    usrOp = -1
+    while(usrOp != 0):
+        print("------------------ Vigenere Chiper ------------------")
+        print("0) Close program")
+        print("1) Encript a file")
+        print("2) Decript a file")
+        usrOp = int(input("Choose a option: "))
+
+        if(usrOp == 1):
+            encriptFile(True)
+        elif(usrOp == 2):
+            encriptFile(False)
+        elif(usrOp != 0):
+            print("This is not a valid option")
+
+main()
---
+++ playFair/playFair.py
@@ -0,0 +1,142 @@
+import string
+
+def getFileLines(path):
+    try:
+        f = open(path, "r")
+        lines = list(map(lambda x: x.rstrip().strip(string.punctuation), f.readlines()))
+        f.close()
+        return lines
+    except:
+        raise FileNotFoundError("There is no such file with this name")
+
+def writeLines(path, lines):
+    try:
+        f = open(path, "w")
+        for line in lines: f.write(line + "\n")
+        f.close()
+    except:
+        raise Exception("Something went wrong when writing to the file")
+
+def createMatrixKey(key):
+    key = key.lower()
+    matrix = [[0 for i in range (5)] for j in range(5)]
+    letters_added = []
+    row = 0
+    col = 0
+
+    for letter in key:
+        if letter not in letters_added:
+            matrix[row][col] = letter
+            letters_added.append(letter)
+        else:
+            continue
+        if (col==4):
+            col = 0
+            row += 1
+        else:
+            col += 1
+
+    for letter in range(96,123):
+        if letter==106:
+                continue
+        if chr(letter) not in letters_added:
+            letters_added.append(chr(letter))
+
+    index = 0
+    
+    for i in range(5):
+        for j in range(5):
+            matrix[i][j] = letters_added[index]
+            index+=1
+    
+    return matrix
+
+def indexOf(letter,matrix):
+    for i in range (5):
+        try:
+            index = matrix[i].index(letter)
+            return (i,index)
+        except:
+            continue
+
+def separateSameLetters(lines):
+    for i, line in enumerate(lines):
+        index = 0
+        
+        while(index < len(line)):
+            character1 = line[index]
+            
+            if(index == len(line) - 1):
+                line = line + 'x'
+                index += 2
+                continue
+            
+            character2 = line[index + 1]
+            
+            if (character1 == character2):
+                line = line[:index+1] + 'x' + line[index+1:]
+            
+            index += 2
+        
+        lines[i] = line
+            
+    return lines
+
+def pf(key, lines, encrypt=True):
+    if encrypt:
+        lines = separateSameLetters(lines)
+
+    matrix = createMatrixKey(key)
+
+    for i, line in enumerate(lines):
+        strAux = ""
+        for word in line.lower().split():
+            strAux = strAux + playfair(matrix, word, encrypt) + " "
+        
+        lines[i] = strAux
+    
+    return lines
+        
+
+def playfair(matrix, message, encrypt=True):
+    inc = 1
+    if encrypt==False:
+        inc = -1
+    
+    cipher_text=''
+    
+    for (l1, l2) in zip(message[0::2], message[1::2]):
+        row1,col1 = indexOf(l1,matrix)
+        row2,col2 = indexOf(l2,matrix)
+        if row1==row2:
+            cipher_text += matrix[row1][(col1+inc)%5] + matrix[row2][(col2+inc)%5]
+        elif col1==col2:
+            cipher_text += matrix[(row1+inc)%5][col1] + matrix[(row2+inc)%5][col2]
+        else:
+            cipher_text += matrix[row1][col2] + matrix[row2][col1]
+    
+    return cipher_text
+
+def main():
+    usrOp = -1
+    path = input("Write the path of the desired file: ")
+    lines = getFileLines(path)
+    key = input("What is the key? ")
+
+    while(usrOp != 0):
+        print("------------------ Play Fair ------------------")
+        print("0) Close program")
+        print("1) Encript a file")
+        print("2) Decript a file")
+        usrOp = int(input("Choose a option: "))
+
+        if(usrOp == 1):
+            writeLines("chiper.txt", pf(key, lines))
+            print("Saving encrypted file on chiper.txt")
+        elif(usrOp == 2):
+            writeLines("solution.txt", pf(key, lines, False))
+            print("Saving decrypted file on solution.txt")
+        elif(usrOp != 0):
+            print("This is not a valid option")
+
+main()
